b43d31f32ce477b38f6549f10aefd07b0f3422ff
valery-kiselev <83948789+valery-kiselev@users.noreply.github.com>
2024-09-03 19:21:00 +0400

    Add files via upload

---
+++ module_7_1.py
@@ -0,0 +1,37 @@
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = str(name)
+        self.weight = float(weight)
+        self.category = str(category)
+
+    def __str__(self):
+        return f'{self.name} {self.weight} {self.category}'
+class Shop:
+    __file_name = 'products.txt'
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        file_text = file.read()
+        file.close()
+        return file_text
+
+    def add(self, *products):
+        for i in products:
+            if self.get_products().find(i.name) == -1:
+                file = open(self.__file_name, 'a')
+                file.write(f'{i}\n')
+                file.close()
+            else:
+                print(f'Продукт {i.name} уже есть в магазине')
+
+s1 = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2) # __str__
+
+s1.add(p1, p2, p3)
+
+print(s1.get_products())
+
+
---
+++ module_7_2.py
@@ -0,0 +1,21 @@
+def custom_write(file_name, strings):
+    dictionary = {}
+    file = open(file_name, 'a', encoding='utf-8')
+    j = 1
+    for i in strings:
+        dictionary[(j, file.tell())] = i
+        file.write(f'\n{i}')
+        j += 1
+    return dictionary
+    file.close()
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
